9b43ca5e2ec648e2b28bb16b3a1f1d294153e0fb
Raji Vignesh <vickypalani16@gmail.com>
2025-09-10 06:03:56 -0500

    Selenium Login Page Test Case Automation

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        //Selenium Automation code for the Project Login Page
+        }
+    }
